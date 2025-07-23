29f902ea0671028432f46865d85d52447d9619a6
macham2023 <machamisaac079@gmail.com>
2025-07-23 07:47:17 +0100

    added

---
+++ logical_operators.py
@@ -0,0 +1,21 @@
+#LOGICAL OPERATORS
+#Making reference to the truth table
+'''
+user_score = int(input("Enter score:\n >>>"))
+print("should your grade be A?:", user_score  >= 70 and user_score <= 100)
+print(not("should your grade be A?:", user_score  >= 70 and user_score <= 100))
+
+
+
+
+num1 = 30
+num2 = 50
+print(num1 < num2 and num2 > num1)
+'''
+
+#TASK
+print("Is 5 greater than 2 and is 10 equal to 10?:", 5 > 2 and 10 == 10)
+print("Is 4 not equal to 4 or is 7 less than 2 and 1 greater than 0?:", 4 != 4 or 7 < 2 and 1 > 0)
+print("Reverse the result of whether 3 is equal to 3.?",not 3 == 3)
+print("If it is not true that 10 is greater than 3?",not 10 > 3)
+print("If 8 is greater than 3 and NOT equal to 4.", 8 > 3 and 8 != 4 )
