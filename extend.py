9c8e5ce6e2c69e478559724db732885710679fbd
macham2023 <machamisaac079@gmail.com>
2025-07-28 15:59:38 +0100

    extend  method

---
+++ extend.py
@@ -0,0 +1,11 @@
+#The extend function in python : 
+# extend(): It adds the specified list elements (or any iterable) to the end of the current list.
+
+
+car_names = ["Benz", "Lamborghini", "Lexus", "Camry", "Tesla"]
+
+country_names = ["Brazil", "Nigeria", "Spain", "England", "Cameroon"]
+
+car_names.extend(country_names)
+
+print(car_names)
